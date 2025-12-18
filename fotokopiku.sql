15423e6c9e10bdf124243677dde3ab5123cb35e8
dwi alfa <dwialfaa21@gmail.com>
2025-12-18 14:49:23 +0700

    update db

--- fotokopiku.sql
+++ fotokopiku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Des 2025 pada 02.04
+-- Waktu pembuatan: 18 Des 2025 pada 08.47
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -24,35 +24,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
---
-
-CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `layanan`
 --
 
 CREATE TABLE `layanan` (
   `id_layanan` int(11) NOT NULL,
   `nama_layanan` varchar(100) NOT NULL,
-  `harga_per_halaman` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `operator`
---
-
-CREATE TABLE `operator` (
-  `id_operator` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
+  `harga_per_lembar` decimal(10,2) NOT NULL,
+  `tipe_layanan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -63,10 +42,13 @@ CREATE TABLE `operator` (
 
 CREATE TABLE `transaksi` (
   `id_transaksi` int(11) NOT NULL,
-  `id_operator` int(11) NOT NULL,
-  `id_layanan` int(11) NOT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `nama_pelanggan` varchar(100) NOT NULL,
   `jumlah_lembar` int(11) NOT NULL,
-  `total_harga` int(11) NOT NULL
+  `total_harga` decimal(12,2) NOT NULL,
+  `tipe_layanan` varchar(50) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_layanan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -78,20 +60,13 @@ CREATE TABLE `transaksi` (
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `role` enum('ADMIN','OPERATOR') NOT NULL
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','kasir','user') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`),
-  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indeks untuk tabel `layanan`
@@ -100,47 +75,29 @@ ALTER TABLE `layanan`
   ADD PRIMARY KEY (`id_layanan`);
 
 --
--- Indeks untuk tabel `operator`
---
-ALTER TABLE `operator`
-  ADD PRIMARY KEY (`id_operator`),
-  ADD KEY `id_user` (`id_user`);
-
---
 -- Indeks untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `id_operator` (`id_operator`),
-  ADD KEY `id_layanan` (`id_layanan`);
+  ADD KEY `fk_user` (`id_user`),
+  ADD KEY `fk_layanan` (`id_layanan`);
 
 --
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT untuk tabel `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT untuk tabel `layanan`
 --
 ALTER TABLE `layanan`
   MODIFY `id_layanan` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `operator`
---
-ALTER TABLE `operator`
-  MODIFY `id_operator` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi`
@@ -159,23 +116,11 @@ ALTER TABLE `user`
 --
 
 --
--- Ketidakleluasaan untuk tabel `admin`
---
-ALTER TABLE `admin`
-  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `operator`
---
-ALTER TABLE `operator`
-  ADD CONSTRAINT `operator_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE;
-
---
 -- Ketidakleluasaan untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_operator`) REFERENCES `operator` (`id_operator`),
-  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id_layanan`);
+  ADD CONSTRAINT `fk_layanan` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id_layanan`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
